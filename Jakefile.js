786d100dde6225524512bf9e6c451108926d5bd2
Richard Jackson <rbjack@gmail.com>
2017-11-05 22:37:37 -0800

    Build script checks the node version

--- Jakefile.js
+++ Jakefile.js
@@ -2,9 +2,20 @@
 (function() {
   "use strict";
 
+  const EXPECTED_NODE_VERSION = "v6.11.5";
+
   desc("Default build");
-  task("default", function() {
+  task("default", ["version"], function() {
     console.log("\n\nBUILD OK");
   });
 
+  desc("Check Node versions");
+  task("version", function(){
+    console.log("Checking Node version: .");
+
+    let actualVersion = process.version;
+    if (actualVersion !== EXPECTED_NODE_VERSION) {
+      fail("Incorrect Node version: expected EXPECTED_NODE_VERSION");
+    }
+  });
 }());
